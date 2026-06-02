e4e77433dc54540d03cc7a1b02cfa964a31574d6
MaryRoseJulongbayan22 <maryrosejulongbayan@gmail.com>
2026-06-02 19:08:53 +0800

    tapos nako

--- adapter_layer/app.js
+++ adapter_layer/app.js
@@ -1,26 +1,20 @@
-import express from 'express'
-import 'dotenv/config.js'
-import authRoutes from './routes/authRoutes.js'
+import express from "express";
+import "dotenv/config";
+import authRoutes from "./routes/authRoutes.js";
 
-const app = express()
+const app = express();
 
+app.use(express.json());
 
-app.use(express.json())
-
-
+// request logger first
 app.use((req, res, next) => {
-  console.log(req.path, req.method)
-  next()
-})
+  console.log(req.path, req.method);
+  next();
+});
 
-try {
-  app.listen(process.env.PORT || 5000, () => {
-    console.log(`Listening to port ${process.env.PORT || 5000}...`)
-  })
-} catch (error) {
-  console.log(error)
-}
+// routes before listen
+app.use("/user", authRoutes);
 
-
-
-app.use('/auth', authRoutes)
+app.listen(process.env.PORT || 3001, () => {
+  console.log(`Listening to port ${process.env.PORT || 3001}...`);
+});
